754825c665fc1a9eb140e2ce5399da8205a88246
polina <p-leonova@list.ru>
2019-04-02 14:31:18 +0300

    seek_bar

--- app/src/main/java/com/example/fucking/MainActivity.java
+++ app/src/main/java/com/example/fucking/MainActivity.java
@@ -2,23 +2,54 @@ package com.example.fucking;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.DoubleSummaryStatistics;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
     private static Object TextView ="";
+   
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SeekBar seekBar =  findViewById(R.id.seekBar);
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        SeekBar.setMax(20);
+
 
         TextView = this.findViewById(R.id.editText);
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            private EditText yourTextView;
+
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    if (fromUser) {
+                        String sizeSeekBar;
+                        if (progress >= 0 && progress <= sizeSeekBar.getMax()) {
+
+                            String progressString = String.valueOf(progress * 10);
+                            yourTextView.setText(progressString); // the TextView Reference
+                            seekBar.setSecondaryProgress(progress);
+                            {
+
+            @Override
+            public void onProgressChanged(seekBar, int progress, boolean fromUser) {
 
                 TextView(String.valueOf(progress));
             }
